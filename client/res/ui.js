5b1175f1c9d2d9956245b5a53de5905b8013ee1e
Owain Jones <tehdoomcat@gmail.com>
2013-03-10 01:59:46 +0000

    more swanky ui stuff

--- client/res/ui.js
+++ client/res/ui.js
@@ -1,4 +1,13 @@
-$("a.button").click(function() {
-    $($(this).attr('href')).toggleClass("active");
-    return false;
-});
+function openPage(button) {
+    activeElem = $($(button).attr('href'));
+    isActive = $(activeElem).hasClass('active');
+    $('.active').removeClass('active');
+    if(!isActive) {
+        $(activeElem).addClass('active');
+    }
+}
+
+$("a.button").click(function() { openPage(this); return false; });
+$("a.buttonlike").click(function() { openPage(this); return false; });
+
+setTimeout(function() { $("a.button[href='#menu']:first").click(); }, 2000);
